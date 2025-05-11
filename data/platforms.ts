86a5a9bc4b0621882757153797d77f8aa1924090
jaslynking <jaslyn@modulariti.com>
2025-05-11 12:50:32 -0500

    Add platform configuration data

---
+++ data/platforms.ts
@@ -0,0 +1,10 @@
+import { Platform } from "../types";
+
+export const platforms: Platform[] = [
+  { id: "github", name: "GitHub", url: "https://github.com/" },
+  { id: "reddit", name: "Reddit", url: "https://www.reddit.com/user/" },
+  { id: "medium", name: "Medium", url: "https://medium.com/@" },
+  { id: "devto", name: "Dev.to", url: "https://dev.to/" },
+  { id: "hashnode", name: "Hashnode", url: "https://hashnode.com/@" },
+  { id: "npmjs", name: "npm", url: "https://www.npmjs.com/~" },
+];
